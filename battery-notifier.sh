7ef5b01e733b8f1af3732fcb001fbf82620e7882
Hemanth Kumar Veeranki <hems.india1997@gmail.com>
2016-07-11 17:20:59 +0530

    Added Dependencies

--- battery-notifier.sh
+++ battery-notifier.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+xbacklight -set 50
 while true
 do
-	export DISPLAY=:0.0
 	battery_percentage=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 	if on_ac_power; then
 		if [ "$battery_percentage" -eq 100 ]; then
+			export DISPLAY=:0.0
 			notify-send -i "/usr/local/bin/battery_full.jpg" "Battery full." "Level: ${battery_percentage}% ";
 			espeak "Please Remove Charger";
 		fi
 	else
 		if [ "$battery_percentage" -lt 10 ]; then
+			export DISPLAY=:0.0
 			notify-send -i "/usr/local/bin/battery_low.jpg" "Battery Low." "Level: ${battery_percentage}% ";
 			espeak "Please Switch on Charger";
 		fi
